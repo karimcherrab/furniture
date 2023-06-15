c2fe1431475d763085f548a319fd1d059e1a7f4c
karimcherrab <ma.cherrab@gmail.com>
2023-06-15 18:14:58 +0100

    dep

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import NewItems from './components/NewItems';
-import FeaturesSecond from './components/FeaturesSecond';
-import Products from './components/Products';
-import Testimonial from './components/Testimonial';
-import Newsletter from './components/Newsletter';
-import Footer from './components/Footer';
-import Header from './components/Header';
 
 // import components
+import { Route , Routes } from "react-router-dom";
+
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-  <div className='w-full maw-w-[1440px] mx-auto bg-white ' >
-    
-    <Header/>
 
-    <Hero/>
-    <Features/>
-    <NewItems/>
-    <FeaturesSecond/>
-    <Products/>
-    <Testimonial/>
-    <Newsletter/>
-    <Footer/>
 
-  </div>
+      
+    <Routes>
+        
+    <Route path="/furniture" element={<Home/>}/>
+
+  </Routes>
+ 
   )
  
 };
